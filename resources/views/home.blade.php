155ec979f0c79c870ce81f5d89cf6e94f1e19a22
rsnows <rsnows@gmail.com>
2022-12-06 12:43:42 -0300

    Initial commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,104 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Roberto Bagulhos</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css" integrity="sha512-1PKOgIY59xJ8Co8+NE6FZ+LOAZKjy+KY8iq0G4B3CyeY6wYHN3yt9PW0XpSriVlkMXe40PTKnXrLnZ9+fkDaog==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+</head>
+<body>
+    <nav class="navbar navbar-light navbar-expand-md bg-light pl-5 pr-5 mb-5">
+        <a href="#" class="navbar-brand">Roberto Bagulhos></a>
+        <div class="collapse navbar-collapse">
+            <div class="navbar-nav">
+                <a href="#" class="nav-link">HOME</a>
+                <a href="#" class="nav-link">Categorias</a>
+                <a href="#" class="nav-link">Cadastrar</a>
+            </div>
+        </div>
+        <a href="#" class="btn btn-sm"><i class="fa fa-shopping-cart"></i></a>
+    </nav>  
+    
+    <div class="container">
+        <div class="row">
+            <div class="col-3 mb-3">
+                <div class="card">
+                    <img src="{{ asset('images/produto1.jpg')" class="card-img-top" />
+                        <div class="card-body">
+                            <h6 class="card-title">Produto 1</h6>
+                            <a href="#" class="btn btn-sm btn-secondary">Adicionar Item</a>
+                        </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-3 mb-3">
+                <div class="card">
+                    <img src="{{ asset('images/produto2.jpg')" class="card-img-top" />
+                        <div class="card-body">
+                            <h6 class="card-title">Produto 1</h6>
+                            <a href="#" class="btn btn-sm btn-secondary">Adicionar Item</a>
+                        </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-3 mb-3">
+                <div class="card">
+                    <img src="{{ asset('images/produto3.jpg')" class="card-img-top" />
+                        <div class="card-body">
+                            <h6 class="card-title">Produto 1</h6>
+                            <a href="#" class="btn btn-sm btn-secondary">Adicionar Item</a>
+                        </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-3 mb-3">
+                <div class="card">
+                    <img src="{{ asset('images/produto4.jpg')" class="card-img-top" />
+                        <div class="card-body">
+                            <h6 class="card-title">Produto 1</h6>
+                            <a href="#" class="btn btn-sm btn-secondary">Adicionar Item</a>
+                        </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-3 mb-3">
+                <div class="card">
+                    <img src="{{ asset('images/produto5.jpg')" class="card-img-top" />
+                        <div class="card-body">
+                            <h6 class="card-title">Produto 1</h6>
+                            <a href="#" class="btn btn-sm btn-secondary">Adicionar Item</a>
+                        </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-3 mb-3">
+                <div class="card">
+                    <img src="{{ asset('images/produto6.jpg')" class="card-img-top" />
+                        <div class="card-body">
+                            <h6 class="card-title">Produto 1</h6>
+                            <a href="#" class="btn btn-sm btn-secondary">Adicionar Item</a>
+                        </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+</body>
+</html>
